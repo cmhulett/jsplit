9a421575395452bff2ded0e0e396628f8977fdec
Carl <carlmhulett@gmail.com>
2013-12-14 02:35:17 -0500

    Project description and license

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jsplit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "timer program for speedrun splits on the JVM"
+  :url "https://github.com/cmhulett/jsplit"
+  :license {:name "BSD 2-Clause license"
+            :url "https://raw.github.com/cmhulett/jsplit/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
